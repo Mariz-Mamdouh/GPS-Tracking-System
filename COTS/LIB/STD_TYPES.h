9e6bfb8c157ebfca04191a1610342d7235c034b5
Mariz <marizmamdouh12@gmail.com>
2024-04-29 14:48:06 +0300

    first commit

---
+++ COTS/LIB/STD_TYPES.h
@@ -0,0 +1,10 @@
+#ifndef STD_TYPES_H
+#define STD_TYPES_H
+
+typedef uint8_t                Std_ReturnType;
+#define E_OK              ((Std_ReturnType)1)
+#define E_NOT_OK          ((Std_ReturnType)0)
+
+#define NULL              ((void*)0x0)
+
+#endif /*STD_TYPES_H*/
---
+++ COTS/MCAL/GPIO/GPIO.c
@@ -0,0 +1,46 @@
+/**< LIB */
+#include "BIT_MATH.h"
+#include "tm4c123gh6pm.h"
+/**< MCAL */
+#include "GPIO.h"
+
+
+void GPIO_InitPort(uint8_t PortID)
+{
+
+}
+
+Std_ReturnType GPIO_SetPinDirection(uint8_t PortID, uint8_t PinId , uint8_t PinDirection)
+{
+
+}
+
+Std_ReturnType GPIO_SetPinValue(uint8_t PortID, uint8_t PinId , uint8_t PinValue)
+{
+
+}
+
+Std_ReturnType GPIO_GetPinValue(uint8_t PortID, uint8_t PinId , uint32_t *ReturnedPinValue)
+{
+
+}
+
+Std_ReturnType GPIO_SetPortDirection(uint8_t PortID, uint8_t PortDirection)
+{
+
+}
+
+Std_ReturnType GPIO_SetPortValue(uint8_t PortID, uint8_t PortValue)
+{
+
+}
+
+Std_ReturnType GPIO_GetPinValue(uint8_t PortID, uint32_t *ReturnedPinValue)
+{
+
+}
+
+Std_ReturnType GPIO_TogglePinValue(uint8_t PortID, uint8_t PinId)
+{
+    
+}
