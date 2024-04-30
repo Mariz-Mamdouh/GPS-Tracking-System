af8e8ed18b1622a17b7894b386aad9b6b4ab6b0b
Mariz <marizmamdouh12@gmail.com>
2024-04-30 13:41:31 +0300

    New Updates

---
+++ COTS/MCAL/UART/UART.h
@@ -0,0 +1,11 @@
+//UART 
+#define UART0_enable       0x0001
+#define GPIOA_enable       0x0001
+#define UART6_enable       0x0040
+#define GPIOD_enable       0x0008
+#define RXFE               0x00000010
+#define TXFF               0x00000020
+#define word_len           0x60    //8-bits
+#define FEN                0x10  //set bit 5 
+#define TXE                0x0100
+#define RXE                0x0200
